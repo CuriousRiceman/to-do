9db9542634292ed8129a4f50b9812c0132e72765
Richman <richmanong4@gmail.com>
2024-04-17 22:59:15 -1000

    Layout done, look into minimizing css in the future

---
+++ src/index.js
@@ -0,0 +1,9 @@
+import './styles.css'; // This will not generate a link to the file in the html file, it will generate the css code in <style> tags if you check dev tools, better to use minimize css
+
+const sidebar = document.querySelector('.sidebar-container');
+const toDoList = document.querySelector('.to-do-list-container');
+
+
+
+
+
